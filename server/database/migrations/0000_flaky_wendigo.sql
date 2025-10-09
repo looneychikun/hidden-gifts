14a5b87eb2f28b21e1cf9a55f5b8892d4c017567
LoONeYChIKuN <andrew@reenan.me>
2025-10-08 19:02:39 -0700

    minor fixes

--- server/database/migrations/0000_flaky_wendigo.sql
+++ server/database/migrations/0000_flaky_wendigo.sql
@@ -1,9 +1,11 @@
 CREATE TABLE `users` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`public_id` text NOT NULL,
 	`name` text NOT NULL,
 	`email` text NOT NULL,
 	`avatar` text NOT NULL,
 	`created_at` integer NOT NULL
 );
 --> statement-breakpoint
+CREATE UNIQUE INDEX `users_public_id_unique` ON `users` (`public_id`);--> statement-breakpoint
 CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,9 +1,11 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
+import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+import crypto from 'node:crypto';
 
 export const users = sqliteTable('users', {
   id: integer('id').primaryKey({autoIncrement: true}),
+  publicId: text('public_id').unique().notNull().$defaultFn(() => crypto.randomUUID()),
   name: text('name').notNull(),
   email: text('email').notNull().unique(),
   avatar: text('avatar').notNull(),
-  createdAt: integer('created_at', { mode: 'timestamp' }).notNull()
+  createdAt: integer('created_at', { mode: 'timestamp' }).notNull().$defaultFn(() => new Date())
 })
